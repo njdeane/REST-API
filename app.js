23ab2932ce49811ce3e00b00da43153c0109e369
njdeane <njdeane7@gmail.com>
2020-11-22 18:18:46 +1000

    complete API with all API http verbs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// required modules
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const ejs = require("ejs");
+//jshint esversion:6
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 // new instance of express app
 const app = express();
@@ -14,50 +15,124 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 // using the public folder to store static files eg: images etc
 app.use(express.static("public"));
 
 // setting up mongoDB
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
-// creating schema for articles collection so I can make models 
-const articlesSchema = {
+// creating schema for articles collection so I can make models
+const articleSchema = {
   title: String,
   content: String
 };
 
 // articles model
-const Article = mongoose.model("Article", articlesSchema);
+const Article = mongoose.model("Article", articleSchema);
+
+///////////////////////////////////Requests Targetting all Articles////////////////////////
+
+app.route("/articles")
 
 // get all articles (on localhost:3000/articles)
-app.get("/articles", function(req, res){
+.get(function(req, res){
   Article.find(function(err, foundArticles){
     if (!err) {
-    res.send(foundArticles);
+      res.send(foundArticles);
     } else {
       res.send(err);
     }
   });
-});
+})
 
 // post request for localhost:3000/articles (adding article via postman)
-app.post("/articles", function(req, res){ 
+.post(function(req, res){
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
   });
+
   newArticle.save(function(err){
     if (!err){
-      res.send("Successfully added a new article");
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+// deletes all articles in database
+.delete(function(req, res){
+
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles.");
     } else {
       res.send(err);
     }
   });
 });
 
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
+.put(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated the selected article.");
+      }
+    }
+  );
+})
+
+.patch(function(req, res){
+
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
 
 // sets app to listen on port 3000
-app.listen(3000, function(){
-  console.log("Server running on port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
